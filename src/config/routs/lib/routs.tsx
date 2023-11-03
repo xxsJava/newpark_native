beb87300385d6feaeba31e4ab55fce611c95a3fd
xxsJava <74550553+xxsJava@users.noreply.github.com>
2023-11-03 14:38:00 +0800

    xxs 路由优化

--- src/config/routs/lib/routs.tsx
+++ src/config/routs/lib/routs.tsx
@@ -1,37 +1,31 @@
-import { createNavigationContainerRef } from '@react-navigation/native';
-import { StackActions, CommonActions } from '@react-navigation/native';
 import routsConfig from '../../routs-config';
 import { FilterManager } from './filter-manager';
+import { navigationRefs } from '../NavigationContainer';
 
 
 /*
  * @Author: xxs
  * @Date: 2023-10-30 09:48:45
- * @LastEditTime: 2023-10-30 11:28:00
+ * @LastEditTime: 2023-11-03 12:27:36
  * @FilePath: \newpark_native\src\config\routs\lib\routs.tsx
  * @Description: 路由跳转
  */
-export class Router {
+export default class Router {
     filterManager!: FilterManager;
     startLen!: number;
-    constructor() {
-    //   this.filterManager = {};
-    //   this.startLen = 0;
-    }	
   
     //跳转函数	
-    push(name: string, params?: object | undefined){
+    navigateRout(name: string, params?: any){
         // 声明目标函数
         function targetFun() {
-          const pushAction = StackActions.push(name, params);
-          
-          navigationRef.dispatch(pushAction);
+          console.log('执行跳转')
+          navigationRefs.current?.navigate(name, params);
         }
         this.execute(name, targetFun);
     }
     
-    execute(name: string, targetFun: { (): void; (): void; }) {
-        
+    execute(name: string, targetFun: { (): void; (): void; }) {  
+      //放行路由 
       let route = routsConfig.Record;
   
       if (route) {
@@ -42,9 +36,6 @@ export class Router {
           interceptors.forEach((element: { [x: string]: any; }) => {
             interceptorClazzs.push(element["clazz"]);
           });
-  
-          const state = navigationRef.getState();
-          this.startLen = state ? state.routes.length : 1;
   
           // 过滤管理器
           this.filterManager = new FilterManager(interceptorClazzs, targetFun);
@@ -68,19 +59,7 @@ export class Router {
      * 清栈
      */
     clearStack() {
-      navigationRef.dispatch(state => {
-        const routes = state.routes.filter((r, index) => {
-          return index < this.startLen;
-        });
-  
-        return CommonActions.reset({
-          ...state,
-          routes,
-          index: routes.length - 1,
-        });
-      });
+      
     }
   }
   
-  export const navigationRef = createNavigationContainerRef();
-  export let router = new Router();
