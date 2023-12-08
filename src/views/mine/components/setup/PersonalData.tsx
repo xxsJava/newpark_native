8d69cb36a9628e2ad4a1c983130262d9f9cb0310
xxsJava <74550553+xxsJava@users.noreply.github.com>
2023-12-08 17:45:07 +0800

    cxr  优化个人中心页面

---
+++ src/views/mine/components/setup/PersonalData.tsx
@@ -0,0 +1,22 @@
+/**
+ * 代码描述: 个人资料页面  设置
+ * 作者:cxr
+ * 修改时间:2023/12/08 14:40:11
+ */
+
+import React from "react";
+import { View,Text,StyleSheet } from "react-native";
+
+const PersonalDataView = () => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+export default PersonalDataView;
+
+const styles = StyleSheet.create({
+
+})
