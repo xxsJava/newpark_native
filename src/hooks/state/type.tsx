41996a7d2514122b1ee52467d6fe82986337bd36
xxsJava <74550553+xxsJava@users.noreply.github.com>
2024-02-22 14:46:45 +0800

    xxs18 群聊图片,角标

--- src/hooks/state/type.tsx
+++ src/hooks/state/type.tsx
@@ -1,11 +1,11 @@
 /*
  * @Author: xxs
  * @Date: 2023-10-31 17:43:59
- * @LastEditTime: 2023-11-01 11:41:38
+ * @LastEditTime: 2024-02-22 13:56:13
  * @FilePath: \newpark_native\src\hooks\state\type.tsx
  * @Description: 定义状态类型
  */
 export interface CounterContextType {
-  phone: string;
-  incrementsetPhone: (phoen: string) => void;
+  corner: number;
+  incrementsetCorner: (corner:number) => void;
 }
--- src/routes/stacker/index.tsx
+++ src/routes/stacker/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: xxs
  * @Date: 2023-10-26 09:38:45
- * @LastEditTime: 2024-02-19 18:46:01
+ * @LastEditTime: 2024-02-22 14:29:37
  * @FilePath: \newpark_native\src\routes\stacker\index.tsx
  * @Description: desc
  */
@@ -9,12 +9,14 @@ import { useTranslation } from 'react-i18next';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Animatable from 'react-native-animatable';
 
 import { Image, Text } from '@gluestack-ui/themed';
 import { Dimensions, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { DeviceEvent } from '../../config/listener';
 import routsConfig from '../../config/routs-config';
+import { useCounter } from '../../hooks/state';
 const windowWidth = Dimensions.get('window').width;
 // import { Stagger, useDisclose} from 'native-base';
 /*
@@ -28,6 +30,18 @@ export const BommonTab = () => {
   const Tab = createBottomTabNavigator();
   const [isVisible, setIsVisible] = useState(false);
   const {t} = useTranslation();
+
+  const { corner,incrementsetCorner } = useCounter();
+
+  useEffect(() =>{
+
+    DeviceEvent.addListener('onRecvNewMessage', async resp => {
+      // incrementsetCorner();
+      console.log('corner--------->',corner);
+      incrementsetCorner(corner);
+    })
+
+  },[])
 
   return (
     <>
@@ -49,23 +63,30 @@ export const BommonTab = () => {
           if (key === 'Routes') {
             const subArray = Object.entries(value);
             return subArray.map(([routeKey, routeValue]) => {
+              const option:any = {
+                tabBarLabel: t(routeValue.label),
+                tabBarIcon: ({focused, size, color}:any) => {
+                  let iconName = focused
+                    ? routeValue.SelectedIcon
+                    : routeValue.UnSelectedIcon;
+                  return (
+                    <Ionicons name={iconName} size={size} color={color} />
+                  );
+                },
+              };
+
+              //添加角标
+              if(routeKey === 'Socializing'){
+                option['tabBarBadge'] = corner;
+              }
+
               // 路由遍历
               return (
                 <Tab.Screen
                   name={routeKey}
                   key={routeKey}
                   component={routeValue.component}
-                  options={{
-                    tabBarLabel: t(routeValue.label),
-                    tabBarIcon: ({focused, size, color}) => {
-                      let iconName = focused
-                        ? routeValue.SelectedIcon
-                        : routeValue.UnSelectedIcon;
-                      return (
-                        <Ionicons name={iconName} size={size} color={color} />
-                      );
-                    },
-                  }}
+                  options={option}
                 />
               );
             });
