2ad36f5acc0fba54d7962cebc356d138b4d325d1
xxsJava <74550553+xxsJava@users.noreply.github.com>
2024-05-10 17:04:12 +0800

    zhn

--- src/views/socializing/components/myGroup/index.tsx
+++ src/views/socializing/components/myGroup/index.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import MyCreScreen from "../page/MyCreScreen";
 import MyJoinScreen from "../page/MyJoinScreen";
 
+
 const Tab = createMaterialTopTabNavigator();
 
 const MyGoroup = () =>{
     return(
-        <Tab.Navigator>
+         <Tab.Navigator>
             <Tab.Screen name="我创建的" component={MyCreScreen} />
             <Tab.Screen name="我加入的" component={MyJoinScreen} />
         </Tab.Navigator>
